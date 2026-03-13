a2baadd9ca89f87598a4209f7f0df0877db44dc8
pranit-gade-23 <pranitgade44@gmail.com>
2026-03-13 12:06:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const text = ["Full Stack Python Developer", "Web Developer", "AI Enthusiast"];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+}
+
+setTimeout(type,150);
+
+}
+
+type();
+
+function darkMode(){
+document.body.classList.toggle("dark");
+}
